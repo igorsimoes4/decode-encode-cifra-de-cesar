8b72e864ed94cb055711ad702a1314f8ff5e01e7
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-08-30 20:37:14 -0300

    modificando os nomes das variaveis

--- criptografa.py
+++ criptografa.py
@@ -2,12 +2,12 @@ key = 20
 result = ''
 frase = "uma frase qualquer que foi criada"
 
-for bola in frase:
-    if bola.islower():  # Verifica se é uma letra minúscula
-        urubu = ord(bola)
-        enc = (urubu - ord('a') + key) % 26 + ord('a')  # Realiza o deslocamento apenas entre as minúsculas
+for letra in frase:
+    if letra.islower():  # Verifica se é uma letra minúscula
+        index_letra = ord(letra)
+        enc = (index_letra - ord('a') + key) % 26 + ord('a')  # Realiza o deslocamento apenas entre as minúsculas
         result = result + chr(enc)
     else:
-        result = result + bola  # Mantém espaços e outros caracteres inalterados
+        result = result + letra  # Mantém espaços e outros caracteres inalterados
 
 print(result)
